c1ad8dde4de411ca67dae00d53c9f7ca86ef021c
MiroslavaTatsenko <tmira@ukr.net>
2018-08-14 23:22:33 +0300

    create page login and singin

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,14 @@
 import os
 from app import app
-from flask import session
+from flask import session, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-#DATABASE_URL = "postgres://kmkhjzcckmmadd:0ccdd910e2a2984001d63285cb5c2e60d1bcd5fdf0f6aa9147836066404f4aaf@ec2-54-217-235-16.eu-west-1.compute.amazonaws.com:5432/d63tuv25mgl2nv"
+DATABASE_URL = "postgres://kmkhjzcckmmadd:0ccdd910e2a2984001d63285cb5c2e60d1bcd5fdf0f6aa9147836066404f4aaf@ec2-54-217-235-16.eu-west-1.compute.amazonaws.com:5432/d63tuv25mgl2nv"
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+#f not os.getenv(DATABASE_URL):
+#   raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -16,10 +16,14 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+#engine = create_engine(os.getenv("DATABASE_URL"))
+#db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
-def index():
-    return "Project 1: TODO"										
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/singin")
+def singin():
+	return render_template("singin.html")										
