22a63c4447d6b9cd5dd4f5f29df295d4b4f292d0
MiroslavaTatsenko <tmira@ukr.net>
2018-08-16 23:09:52 +0300

    create db

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,12 @@
 import os
-from app import app
+from app import app, db
 from flask import session, render_template, request, flash
-from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+
 
 from forms.enterForms import LoginForm, SinginForm
+from models import Users
 
-DATABASE_URL = "postgres://kmkhjzcckmmadd:0ccdd910e2a2984001d63285cb5c2e60d1bcd5fdf0f6aa9147836066404f4aaf@ec2-54-217-235-16.eu-west-1.compute.amazonaws.com:5432/d63tuv25mgl2nv"
-# Check for environment variable
-#if not os.getenv(DATABASE_URL):
-#   raise RuntimeError("DATABASE_URL is not set")
-
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# Set up database
-#engine = create_engine(os.getenv("DATABASE_URL"))
-#db = scoped_session(sessionmaker(bind=engine))
+#db.create_all()
 
 @app.route("/")
 def index():
@@ -46,6 +33,10 @@ def singin():
 		password=request.form["password"]
 		email=request.form["email"]
 		if form.validate():
+			user = Users(name, email)
+			print(user)
+			db.session.add(user)
+			db.session.commit()
 			# Save the comment here.
 			flash('Thanks for registration ' + name)
 		else:
--- config.py
+++ config.py
@@ -1,2 +1,6 @@
+SQLALCHEMY_DATABASE_URI = "postgres://oajlecdrqctkdc:1cfac06095f7c488e34a179da416cbcdc9bc448d839313c9754785caa2b14634@ec2-107-21-236-219.compute-1.amazonaws.com:5432/d73fpsr4h3g909"
+SESSION_PERMANENT = False
+SESSION_TYPE = "filesystem"
+
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
